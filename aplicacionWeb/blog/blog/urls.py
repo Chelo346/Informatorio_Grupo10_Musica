54c4def61f80bcaa6d9e5245ea8389a4e8744822
Gonzalo Pérez de Eulate <g.p.e.140@gmail.com>
2023-12-13 15:37:49 -0300

    Cambios a partir de clase 21-11

--- aplicacionWeb/blog/blog/urls.py
+++ aplicacionWeb/blog/blog/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# Import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.saludo, name='saludo'),
+    path("despedir/", views.despedida, name='despedir'),
+    path("nombre/", views.nombre, name='nombre'),
 ]
