ba4c3fc4cf24e2de35cd8dae42276d4c3f08661e
[chelo346] <producciones_francisco@hotmail.com>
2023-12-15 00:15:47 -0300

    Se saco carpeta entorno virtual

--- aplicacionWeb/blog/settings.py
+++ aplicacionWeb/blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.posts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,6 +81,17 @@ DATABASES = {
     }
 }
 
+# para usar bd mysql (pip install mysqlclient)
+# DATABASES = {
+#     "default":{
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "nombreBD",
+#         "USER": "root",
+#         "PASSWORD": "1234",
+#         "HOST": "localhost",
+#         "PORT": "3604",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -103,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "es"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "America/Argentina/Buenos_Aires"
 
 USE_I18N = True
 
@@ -116,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
