59bcff464772e4a37703ff8f980f58e530d3ecc5
[chelo346] <producciones_francisco@hotmail.com>
2023-12-18 16:08:17 -0300

    Se agrego funciones en urls

---
+++ aplicacionWeb/blog/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+#  render
+from django.shortcuts import render
+# Cada vez que agrego una funcion nueva tiene que ser llamado desde url.
+def saludo(request):
+    return render(request, "index.html")
