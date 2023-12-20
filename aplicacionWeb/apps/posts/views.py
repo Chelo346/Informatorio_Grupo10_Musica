e7ec8ba28986c923dddaaa9661aab8ac78ba5503
[chelo346] <producciones_francisco@hotmail.com>
2023-12-20 16:21:45 -0300

    Se agrego archivo css

--- aplicacionWeb/apps/posts/views.py
+++ aplicacionWeb/apps/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_post(request):
+    return render(request, "home.html")
+
+def categorias_post(request):
+    return render(request, "categorias.html")
+
+def post(request):
+    return render(request, "post.html")
