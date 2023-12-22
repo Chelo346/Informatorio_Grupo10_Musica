cdc176e3cce5bbcf315df71194b8cf305eb492f1
[chelo346] <producciones_francisco@hotmail.com>
2023-12-22 02:43:42 -0300

    Se corrigio base de datos

--- aplicacionWeb/usuarios/models.py
+++ aplicacionWeb/usuarios/models.py
@@ -8,17 +8,17 @@ class Usuario(AbstractUser):
     domicilio = models.CharField(max_length = 255, blank = True, null = True)
     es_admin = models.BooleanField(default=False)
 
-    groups = models.ManyToManyField(
-        'auth.Group',
-        blank=True,
-        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
-        related_name="%(app_label)s_%(class)s_set",   # Cambia esto aquí
-        related_query_name="%(app_label)s_%(class)ss", # Cambia esto aquí
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        blank=True,
-        help_text='Specific permissions for this user.',
-        related_name="%(app_label)s_%(class)s_set",   # Cambia esto aquí
-        related_query_name="%(app_label)s_%(class)ss", # Cambia esto aquí
-    )
+    # groups = models.ManyToManyField(
+    #     'auth.Group',
+    #     blank=True,
+    #     help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+    #     related_name="%(app_label)s_%(class)s_set",   # Cambia esto aquí
+    #     related_query_name="%(app_label)s_%(class)ss", # Cambia esto aquí
+    # )
+    # user_permissions = models.ManyToManyField(
+    #     'auth.Permission',
+    #     blank=True,
+    #     help_text='Specific permissions for this user.',
+    #     related_name="%(app_label)s_%(class)s_set",   # Cambia esto aquí
+    #     related_query_name="%(app_label)s_%(class)ss", # Cambia esto aquí
+    # )
