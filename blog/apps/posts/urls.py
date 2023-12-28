80f1e42abacd447647d48d67eb095c4cf4991070
[chelo346] <producciones_francisco@hotmail.com>
2023-12-28 18:27:50 -0300

    Nuevo proyecto

---
+++ blog/apps/posts/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+
+# Import views
+from . import views
+
+app_name = "posts"
+
+urlpatterns = [
+    path("", views.home_post, name="home_posts"),
+    # path("categorias", views.categorias_post, name="categorias_posts"),
+    # Posteos
+    path("posteos/", views.post_realizado, name="post_realizado"),
+    # post detail
+    path("post_detail/<int:post_id>", views.post_detail, name="post_detail"),
+    # urls comentario
+    path("comentario", views.comentar_posteo, name="comentar"),
+    # url borrar comentario
+    path(
+        "Borrar/<int:pk>",
+        views.Borrar_Comentario.as_view(),
+        name="borrar_comentario",
+    ),
+    path(
+        "Modificar/<int:pk>",
+        views.Modificar_Comentario.as_view(),
+        name="modificar_comentario",
+    ),
+    # url para crear posts
+    path("cargar/", views.Cargar_Post.as_view(), name="cargar_post"),
+]
